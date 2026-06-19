d67b0680d9676eef9b610f2ee6f0d278aee580e3
DanilRoiven <roiven435@gmail.com>
2026-06-19 14:46:47 +0500

    refactor(weather_api): remove unneeded mockData

--- weather_api/src/routers/weather.py
+++ weather_api/src/routers/weather.py
@@ -11,34 +11,8 @@ limiter = Limiter(key_func=get_remote_address, storage_uri=f"redis://{os.getenv(
 
 router = APIRouter()
 
-is_mock = False
-
-class MockData:
-    def __init__(self, name: str, temp: float, feels_like: float, weather_desc: str, wind: float, humidity: int):
-        self.name = name
-        self.temp = temp
-        self.feels_like = feels_like
-        self.weather_desc = weather_desc
-        self.wind = wind
-        self.humidity = humidity
-
-dataUfa = MockData("Уфа", 24.66, 24.25, "облачно с прояснениями", 5.2, 48)
-dataMoscow = MockData("Москва", 30.82, 26.85, "ясно", 2.5, 60)
-dataSPB = MockData("Санкт-Петербург", 19.13, 18.45, "пасмурно", 12.4, 70)
-
 @router.get("/weather")
 @limiter.limit("10/minute")
 async def root(request: Request, name: str):
-    if is_mock:
-        match name:
-            case "Уфа":
-                return dataUfa
-            case "Москва":
-                return dataMoscow
-            case "Санкт-Петербург":
-                return dataSPB
-            case _:
-                return dataUfa
-    else:
-        data = await service.get_weather_data(name)
-        return data
+    data = await service.get_weather_data(name)
+    return data
